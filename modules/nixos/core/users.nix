4718e22d9f93ccfbbfb2e9999eb960ac54ef509d
Salar Kalantari <salar.kalantari@gmail.com>
2025-12-02 15:29:57 +0330

    Define a primary group for user salar

--- modules/nixos/core/users.nix
+++ modules/nixos/core/users.nix
@@ -1,9 +1,10 @@
-# modules/nixos/core/users.nix
 { config, lib, pkgs, ... }:
-{
 
+{
+  # --- Shell / login configuration ---
   programs.zsh = {
     enable = true;
+
     interactiveShellInit = ''
       # Auto-start dwl + dwlb on tty1
       if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = "1" ]; then
@@ -12,19 +13,34 @@
     '';
   };
 
+  # --- Users & groups ---
+  users = {
+    # Primary group for user "salar"
+    groups.salar = {
+      # Optional: pin a GID if you like
+      # gid = 100;
+    };
 
-  users.users = {
-    salar = {
+    users.salar = {
       isNormalUser = true;
+      # Explicit primary group
+      group = "salar";
+
       home = "/home/salar";
       createHome = true;
       description = "Salar Kalantari";
+
       extraGroups = [
-        "wheel" "audio" "video" "networkmanager" "input" "seat"
+        "wheel"
+        "audio"
+        "video"
+        "networkmanager"
+        "input"
+        "seat"
       ];
+
       shell = pkgs.zsh;
     };
   };
-
 }
 
