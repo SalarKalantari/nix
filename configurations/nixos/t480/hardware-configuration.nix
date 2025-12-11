6ef23837f8edbf41f29c8ae660f5f76d9f013f95
Salar Kalantari <salar.kalantari@gmail.com>
2025-12-11 19:32:42 +0330

    Add hardware-configuration for ThinkPad T480 laptop

--- configurations/nixos/t480/hardware-configuration.nix
+++ configurations/nixos/t480/hardware-configuration.nix
@@ -8,24 +8,24 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8676b738-5d79-4dae-a55f-01760d78c132";
+    { device = "/dev/disk/by-uuid/ca3a7249-1fd2-43f0-9fc5-dec51031aae9";
       fsType = "xfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/43E3-EDF9";
+    { device = "/dev/disk/by-uuid/8E73-709C";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/e4c0346f-dc40-4943-a866-a4f9e1d1cf22"; }
+    [ { device = "/dev/disk/by-uuid/4eba5ddf-b02e-4129-b54b-bcea2e61b8cf"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -33,7 +33,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
